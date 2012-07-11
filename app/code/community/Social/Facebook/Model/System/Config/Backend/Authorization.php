bd4ffd2aea5173832f4fd8b4104ea5a032b6b1d1
John Jawed (JJ) <jawed@php.net>
2012-07-11 16:37:05 -0400

    Add authorization model

---
+++ app/code/community/Social/Facebook/Model/System/Config/Backend/Authorization.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Social
+ * @package     Social_Facebook
+ * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Social_Facebook_Model_System_Config_Backend_Authorization extends Mage_Core_Model_Config_Data
+{
+
+    /**
+     * Workaround to disable "delete" checkbox near uploaded file
+     * @todo going to be removed after adding functionality to revoke authorization data
+     *
+     * @return bool
+     */
+    public function getValue()
+    {
+        return false;
+    }
+
+    /**
+     * Take actions to process and save submitted authorization file
+     *
+     * @return Social_Facebook_Model_System_Config_Backend_Authorization
+     */
+    protected function _beforeSave()
+    {
+        if (!isset($_FILES['groups']['tmp_name'][$this->getGroupId()]['fields'][$this->getField()]['value'])) {
+            return $this;
+        }
+
+        $tmpPath = $_FILES['groups']['tmp_name'][$this->getGroupId()]['fields'][$this->getField()]['value'];
+
+        if ($tmpPath && file_exists($tmpPath)) {
+            if (!filesize($tmpPath)) {
+                Mage::throwException(Mage::helper('xcom_xfabric')->__('XFabric authorization file is empty.'));
+            }
+
+            try {
+                $json = file_get_contents($tmpPath);
+                $auth = json_decode($json);
+                if(empty($auth->authorizations)) {
+                    Mage::throwException('Invalid authorization file!');
+                }
+                foreach($auth->authorizations as $token_info) {
+                    if(strtolower($token_info->type)!='tenant') {
+                        continue;
+                    }
+                    Mage::getConfig()->saveConfig(Social_Facebook_Model_Facebook::XML_PATH_FABRIC_URL, $auth->fabricUrl, 'default', 0);
+                    Mage::getConfig()->saveConfig(Social_Facebook_Model_Facebook::XML_PATH_CAP_TOKEN, $token_info->bearerToken, 'default', 0);
+                    Mage::getConfig()->saveConfig(Social_Facebook_Model_Facebook::XML_PATH_TENANT_NAME, $token_info->tenantName, 'default', 0);
+                    break;
+                }
+                Mage::getConfig()->saveConfig(Social_Facebook_Model_Facebook::XML_PATH_RAW_AUTH_JSON, $json, 'default', 0);
+                Mage::getConfig()->cleanCache();
+            } catch (Mage_Core_Exception $e) {
+                Mage::throwException($e->getMessage());
+            }
+
+            unlink($tmpPath);
+        }
+
+        return $this;
+    }
+}
